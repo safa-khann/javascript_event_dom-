4c03b93a9bdaff1d44faa4fa0a510026c6bbcd4b
safa-khann <163485177+safa-khann@users.noreply.github.com>
2024-03-22 11:13:28 -0700

    Add files via upload

---
+++ event1_js.js
@@ -0,0 +1,21 @@
+let eventCount = 0;
+
+function update(){
+    console.log("event is fired");
+    eventCount++;
+    document.getElementById("eventCount").innerText = eventCount;
+}
+//i want to create variable
+
+var x = 10;
+var y = 10;
+
+console.log(x*y);
+
+function sum(a,b){
+    return a+b;
+
+    
+}
+
+
---
+++ event2_js.js
@@ -0,0 +1,8 @@
+function update(){
+    console.log(" Updating results");
+
+var input = document.getElementById('myinput');
+var output = document.getElementById('output');
+output.innerHTML = input.value;
+}
+
